2c7c9f7ca50f9df3e2c96ab0aad0cf0044885105
faithchep <faithchepkoech40@gmail.com>
2023-09-06 15:29:01 +0300

    Add function to save and list the morgue strengths

--- omod/src/main/java/org/openmrs/module/morgueapp/fragment/controller/MorgueDetailFragmentController.java
+++ omod/src/main/java/org/openmrs/module/morgueapp/fragment/controller/MorgueDetailFragmentController.java
@@ -1,10 +1,21 @@
 package org.openmrs.module.morgueapp.fragment.controller;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.hospitalcore.HospitalCoreService;
+import org.openmrs.module.hospitalcore.model.EhrMorgueStrength;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
 
 public class MorgueDetailFragmentController {
 
-    public void controller(FragmentModel model) {}
+    public void controller(FragmentModel model) {
+        model.addAttribute("units",Context.getService(HospitalCoreService.class).getEhrMorgueStrength());
+    }
 
     public void enrollBodyDetails() {
 
@@ -12,5 +23,29 @@ public class MorgueDetailFragmentController {
 
     public void admitBodyDetails() {
 
+    }
+    public void addMorgueUnits(@RequestParam(value = "morgueName") String morgueName,
+                                      @RequestParam(value = "description") String description,
+                                      @RequestParam(value = "strength") int strength,
+                                      @RequestParam(value = "retired") int retired){
+        HospitalCoreService hospitalCoreService = Context.getService(HospitalCoreService.class);
+        EhrMorgueStrength ehrMorgueStrength = new EhrMorgueStrength();
+        ehrMorgueStrength.setMorgueName(morgueName);
+        ehrMorgueStrength.setDescription(description);
+        ehrMorgueStrength.setStrength(strength);
+        ehrMorgueStrength.setRetired(retired);
+        ehrMorgueStrength.setCreatedBy(Context.getAuthenticatedUser().getId());
+        ehrMorgueStrength.setCreatedOn(new Date());
+        hospitalCoreService.saveEhrMorgueStrength(ehrMorgueStrength);
+
+    }
+    public List<SimpleObject> fetchUnitDetails(UiUtils uiUtils) {
+        HospitalCoreService hospitalCoreService = Context.getService(HospitalCoreService.class);
+
+        List<SimpleObject> units = null;
+        List<EhrMorgueStrength> ehrMorgueStrengths = hospitalCoreService.getEhrMorgueStrength();
+        units = SimpleObject.fromCollection(ehrMorgueStrengths, uiUtils, "ehrMorgueStrengthId", "morgueName", "description","strength","retired");
+
+        return units;
     }
 }
